faffb49d8b240efca23c47c091b23d45a71bad12
Aaryan Dhand <aaryandhand@gmail.com>
2024-02-29 14:33:39 -0700

    fgdsgfdsghaes ga

--- ToDoList.java
+++ ToDoList.java
@@ -1,3 +1,14 @@
-public class ToDoList {
+public class ToDoList implements IToDoList {
+    private List<Task> tasks;
+    private Stack<List<Tasks>> history;
 
+    public ToDoList(){
+        this.tasks = new ArrayList<>();
+        this.history = new Stack<>();
+    }
+
+    @Override
+    public void addTask(Task,task){
+        
+    }
 }
