dbf5c4682c746789addf61bd80d562b4df1f7b4e
Aaryan Dhand <aaryandhand@gmail.com>
2024-02-29 15:07:40 -0700

    dsfaads

--- Task.java
+++ Task.java
@@ -10,12 +10,14 @@ public class Task {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
         Task task = (Task) obj;
         return Objects.equals(id, task.id) &&
-               Objects.equals(title, task.title) &&
-               isCompleted == task.isCompleted; 
+                Objects.equals(title, task.title) &&
+                isCompleted == task.isCompleted;
     }
 
     @Override
@@ -23,8 +25,11 @@ public class Task {
         return Objects.hash(id, title, isCompleted);
     }
 
-    public copy(Task task){
-        Task newTask = (Task) task.clone();
+    public Task copy() {
+        Task newTask = new Task();
+        newTask.setId(this.id);
+        newTask.setTitle(this.title);
+        newTask.setCompleted(this.isCompleted);
         return newTask;
     }
 }
